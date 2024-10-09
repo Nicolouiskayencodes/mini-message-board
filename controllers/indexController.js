4d39f8ae1f50509f9b530aaea71d69f9b5a51ecf
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-10-08 20:14:06 -0700

    Add title index to open message page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,7 +8,8 @@ const selectMessage = (messages) => {
 
   return (req, res) => {
     const message = messages[req.params.index]
-    res.render('open', {user: message.user, text: message.text, added: message.added})
+    const title = 'Message ' + req.params.index;
+    res.render('open', {title: title, user: message.user, text: message.text, added: message.added})
   }
 }
 
